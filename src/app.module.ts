20f5cd5cb0b4bfd376b4daca4aeb6987682185cb
Ndang11 <royaltyhopsin.com>
2026-01-20 16:44:02 +0100

    trying to fix deployed endpoints that are not working

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,13 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
-import { APP_GUARD } from "@nestjs/core";
-import { JwtModule } from "@nestjs/jwt";
 import { MongooseModule } from "@nestjs/mongoose";
+
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { AuthModule } from "./auth/auth.module";
-import { jwtConstants } from "./auth/constants";
-import { AuthGuard } from "./common/guards/auth.guard";
+import { HealthController } from "./health/health.controller";
 import { PostsModule } from "./posts/posts.module";
 import { TenantsModule } from "./tenants/tenants.module";
-import { UsersController } from "./users/users.controller";
 import { UsersModule } from "./users/users.module";
 
 @Module({
@@ -18,31 +15,15 @@ import { UsersModule } from "./users/users.module";
 		ConfigModule.forRoot({
 			isGlobal: true,
 		}),
-		MongooseModule.forRoot(
-			process.env.MONGO_URI ??
-				(() => {
-					throw new Error("MONGO_URI is not defined");
-				})(),
-		),
-		JwtModule.register({
-			global: true,
-			secret: jwtConstants.secret,
-			signOptions: { expiresIn: "7d" },
-		}),
+
+		MongooseModule.forRoot(process.env.MONGO_URI as string),
 
 		AuthModule,
 		UsersModule,
 		TenantsModule,
 		PostsModule,
 	],
-
-	controllers: [AppController, UsersController],
-	providers: [
-		{
-			provide: APP_GUARD,
-			useClass: AuthGuard,
-		},
-		AppService,
-	],
+	controllers: [AppController, HealthController],
+	providers: [AppService],
 })
 export class AppModule {}
--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -4,8 +4,8 @@ import {
 	Injectable,
 	UnauthorizedException,
 } from "@nestjs/common";
-import { JwtService } from "@nestjs/jwt";
-import { Request } from "express";
+import type { JwtService } from "@nestjs/jwt";
+import type { Request } from "express";
 
 @Injectable()
 export class JwtAuthGuard implements CanActivate {
