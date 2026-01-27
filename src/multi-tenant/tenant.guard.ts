04899734f757847f429a8d2af156b61ea8db8f73
Ndang11 <royaltyhopsin.com>
2026-01-27 13:56:55 +0100

    fix: Imported the tenantModule in the multiTenantmODULE and changed the service import from type to normal import

--- src/multi-tenant/tenant.guard.ts
+++ src/multi-tenant/tenant.guard.ts
@@ -4,9 +4,9 @@ import {
 	Injectable,
 	UnauthorizedException,
 } from "@nestjs/common";
-import type { Reflector } from "@nestjs/core";
 import { IS_PUBLIC_KEY } from "../common/decorators/public.decorator";
 import { TenantsService } from "../tenants/tenants.service";
+import { Reflector } from "@nestjs/core";
 
 @Injectable()
 export class TenantGuard implements CanActivate {
