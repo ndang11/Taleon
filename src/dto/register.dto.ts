809d6f28dc97705b12e7794a8c4ba1631b8df061
Ndang11 <royaltyhopsin.com>
2026-02-02 12:53:12 +0100

    resolve all nestjs dependency issues

--- src/dto/register.dto.ts
+++ src/dto/register.dto.ts
@@ -1,10 +1,4 @@
-import {
-	IsEmail,
-	IsNotEmpty,
-	IsString,
-	Matches,
-	MinLength,
-} from "class-validator";
+import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
 
 export class RegisterDto {
 	@IsString()
