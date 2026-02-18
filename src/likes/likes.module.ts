0a49f99993b8abcd5f56baccb59bbb003f757534
Ndang11 <royaltyhopsin.com>
2026-02-18 18:22:50 +0100

    new changes made on movie cards

--- src/likes/likes.module.ts
+++ src/likes/likes.module.ts
@@ -41,7 +41,7 @@ FixedLikeSchema.index({ postId: 1, userId: 1 }, { unique: true });
 		MongooseModule.forFeature([{ name: "Like", schema: FixedLikeSchema }]),
 		forwardRef(() => MultiTenantModule),
 		forwardRef(() => PostsModule),
-		NotificationsModule,
+		forwardRef(() => NotificationsModule),
 	],
 	controllers: [LikesController],
 	providers: [LikesService],
