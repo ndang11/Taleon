0e17a330ddeb0b1873207c28bed8aaf7719b8fcd
Ndang11 <royaltyhopsin.com>
2026-02-18 23:13:42 +0100

    Added User model import to enable updating user counts

--- src/follows/follows.module.ts
+++ src/follows/follows.module.ts
@@ -3,12 +3,16 @@ import { MongooseModule } from "@nestjs/mongoose";
 import { MultiTenantModule } from "../multi-tenant/multi-tenant.module";
 import { NotificationsModule } from "../notifications/notifications.module";
 import { Follow, FollowSchema } from "../schemas/follow.schema";
+import { User, UserSchema } from "../schemas/users.schema";
 import { FollowsController } from "./follows.controller";
 import { FollowsService } from "./follows.service";
 
 @Module({
 	imports: [
-		MongooseModule.forFeature([{ name: Follow.name, schema: FollowSchema }]),
+		MongooseModule.forFeature([
+			{ name: Follow.name, schema: FollowSchema },
+			{ name: User.name, schema: UserSchema },
+		]),
 		MultiTenantModule,
 		NotificationsModule,
 	],
